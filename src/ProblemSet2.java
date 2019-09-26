954170df0cc618ee0cfc32fc7d77a8c4c3c4750b
Matthew Man <mman@ucvts.org>
2019-09-26 08:35:26 -0400

    third commit

--- src/ProblemSet2.java
+++ src/ProblemSet2.java
@@ -27,27 +27,27 @@ public class ProblemSet2 {
          * Prompt the user to enter the following information (in order): first name, last
          * name, grade, age, and hometown.
          */
-         System.out.println("Enter your first name:");
+         System.out.print("Enter your first name: ");
          String firstName = in.nextLine();
 
-         System.out.println("What's your last name?");
+         System.out.print("Enter your last name: ");
          String lastName = in.nextLine();
 
-         System.out.println("Enter your grade:");
+         System.out.print("Enter your grade: ");
          int grade = in.nextInt();
 
-         System.out.println("Enter your age:");
+         System.out.print("Enter your age: ");
          int age = in.nextInt();
          in.nextLine();
 
-         System.out.println("Enter your hometown:");
+         System.out.print("Enter your hometown: ");
          String hometown = in.nextLine();
 
-         System.out.println("NAME     : " + firstName + " " + lastName);
+         System.out.println("\nNAME     : " + firstName + " " + lastName);
          System.out.println("GRADE    : " + grade);
          System.out.println("AGE      : " + age);
          System.out.println("HOMETOWN : " + hometown);
-         in.close();
+
 
 
         /*
@@ -56,7 +56,37 @@ public class ProblemSet2 {
          * Given a dollar amount in the range [0.00, 1.00], print the number of dollar
          * bills, quarters, dimes, nickels, and pennies needed to produce this amount.
          */
+         final int DOLLAR_VALUE = 100;
+         final int QUARTER_VALUE = 25;
+         final int DIME_VALUE = 10;
+         final int NICKEL_VALUE = 5;
+         final int PENNY_VALUE = 1;
 
+         System.out.print("\nEnter a dollar amount: ");
+         double dollarAmount = in.nextDouble();
+
+         int pennyAmount = (int) (dollarAmount * 100);
+
+         int amountDollar = pennyAmount / DOLLAR_VALUE;
+         pennyAmount = pennyAmount % DOLLAR_VALUE;
+
+         int amountQuarter = pennyAmount / QUARTER_VALUE;
+         pennyAmount = pennyAmount % QUARTER_VALUE;
+
+         int amountDime = pennyAmount / DIME_VALUE;
+         pennyAmount = pennyAmount % DIME_VALUE;
+
+         int amountNickel = pennyAmount / NICKEL_VALUE;
+         pennyAmount = pennyAmount % NICKEL_VALUE;
+
+         int amountPenny = pennyAmount / PENNY_VALUE;
+         pennyAmount = pennyAmount % PENNY_VALUE;
+
+         System.out.println("\nDOLLARS  : " + amountDollar);
+         System.out.println("QUARTERS : " + amountQuarter);
+         System.out.println("DIMES    : " + amountDime);
+         System.out.println("NICKELS  : " + amountNickel);
+         System.out.println("PENNIES  : " + amountPenny);
 
 
         /*
@@ -65,7 +95,40 @@ public class ProblemSet2 {
          * Given a dollar amount in the range [0.00, 20.00], print the smallest number of
          * bills and coins needed to produce this amount.
          */
+         final int TEN_VALUE = 1000;
+         final int FIVE_VALUE = 500;
 
+         System.out.print("\nEnter a dollar amount: ");
+         double dollarAmounts = in.nextDouble();
+
+         int pennyAmounts = (int) (dollarAmounts * 100);
+
+         int amountTens = pennyAmounts / TEN_VALUE;
+         pennyAmounts = pennyAmounts % TEN_VALUE;
+
+         int amountFives = pennyAmounts / FIVE_VALUE;
+         pennyAmounts = pennyAmounts % FIVE_VALUE;
+
+         int amountDollars = pennyAmounts / DOLLAR_VALUE;
+         pennyAmounts = pennyAmounts % DOLLAR_VALUE;
+
+         int amountQuarters = pennyAmounts / QUARTER_VALUE;
+         pennyAmounts = pennyAmounts % QUARTER_VALUE;
+
+         int amountDimes = pennyAmounts / DIME_VALUE;
+         pennyAmounts = pennyAmounts % DIME_VALUE;
+
+         int amountNickels = pennyAmounts / NICKEL_VALUE;
+         pennyAmounts = pennyAmounts % NICKEL_VALUE;
+
+         int amountPennys = (int) (pennyAmounts / PENNY_VALUE);
+         pennyAmounts = pennyAmounts % PENNY_VALUE;
+
+         int amountBills = amountTens + amountFives + amountDollars;
+         int amountCoins = amountQuarters + amountDimes + amountNickels + amountPennys;
+
+         System.out.println("\nBILLS : " + amountBills);
+         System.out.println("COINS : " + amountCoins);
 
 
         /*
@@ -74,7 +137,26 @@ public class ProblemSet2 {
          * Given a number of inches, print the equivalent number of miles, yards, feet,
          * and inches.
          */
-
+         // final int INCH_MILE_CONVERSION = 63360;
+         // final int inchesInAYard = 36;
+         // int inchesInAFoot = 12;
+         //
+         // System.out.print("\nEnter a number of inches: ");
+         // int numInches = in.nextInt();
+         //
+         // int numMiles = numInches / inchesInAMile;
+         // numInches = numInches % inchesInAMile;
+         //
+         // int numYards = numInches / inchesInAYard;
+         // numInches = numInches % inchesInAYard;
+         //
+         // int numFeet = numInches / inchesInAFoot;
+         // numInches = numInches % inchesInAFoot;
+         //
+         // System.out.println("\nMILES  : " + numMiles);
+         // System.out.println("YARDS  : " + numYards);
+         // System.out.println("FEET   : " + numFeet);
+         // System.out.println("INCHES : " + numInches);
 
 
         /*
